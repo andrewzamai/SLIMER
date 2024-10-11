1175067a3d95ba10682697450324097ab1156ed6
Andrew <andrewzamai@hotmail.com>
2024-10-11 13:54:40 +0200

    vLLM evaluation

--- src/SLIM-GNER/scripts/eval_SLIM-GNER.sh
+++ src/SLIM-GNER/scripts/eval_SLIM-GNER.sh
@@ -1,4 +1,4 @@
-MODEL_NAME_OR_PATH=merged_models/GNER_wDeG_391x50-LLaMA3
+MODEL_NAME_OR_PATH=merged_models/GNER_wDeG_391x50-LLaMA2
 JSON_TEST_FILE=data/zero-shot-test-wDeG-5NEperPrompt.jsonl
 
 python src/run_predictions_vllm.py \
@@ -7,5 +7,5 @@ python src/run_predictions_vllm.py \
 --max_source_length 2048 \
 --max_new_tokens 2048 \
 --temperature 0 \
---stop_token "<|eot_id|>"
+--stop_token "</s>"
 
