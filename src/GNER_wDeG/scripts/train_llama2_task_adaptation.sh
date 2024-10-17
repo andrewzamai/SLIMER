63138247aea8ad35046e5b76535904d8d624b3ff
Andrew <andrewzamai@hotmail.com>
2024-10-17 10:45:21 +0200

    GNER wo DeG Llama2

--- src/GNER_wDeG/scripts/train_llama2_task_adaptation.sh
+++ src/GNER_wDeG/scripts/train_llama2_task_adaptation.sh
@@ -6,11 +6,10 @@ MODEL_NAME_OR_PATH=meta-llama/Llama-2-7b-chat-hf
 DATA_DIR=data
 DATA_CONFIG_DIR=configs/dataset_configs/task_adaptation_configs
 INSTRUCTION_FILE=configs/instruction_configs/instruction.json
-OUTPUT_DIR=output/GNER-391x50-wDeG-LLaMA2-7B-chat-v2
-PATH_TO_DeG=../data_handlers/questions/pileNER/top391NEs_definitions.json
+OUTPUT_DIR=output/GNER-391xALL-woDeG-LLaMA2-7B-chat
 
 DEEPSPEED_CONFIG=configs/deepspeed_configs/deepspeed_zero1_llama.json
-RUN_NAME=GNER-wDeG-LLaMA2-7B-chat-v2
+RUN_NAME=GNER-391xALL-woDeG-LLaMA2-7B-chat
 
 deepspeed --include="localhost:0" --master_port $port src/run.py \
     --bf16 True --tf32 True \
@@ -21,7 +20,7 @@ deepspeed --include="localhost:0" --master_port $port src/run.py \
     --preprocessing_num_workers 4 \
     --load_best_model_at_end True \
     --metric_for_best_model "eval_average_f1" \
-    --max_eval_samples 100 \
+    --max_eval_samples 250 \
     --greater_is_better True \
     --data_config_dir $DATA_CONFIG_DIR \
     --instruction_file $INSTRUCTION_FILE \
@@ -36,14 +35,14 @@ deepspeed --include="localhost:0" --master_port $port src/run.py \
     --lr_scheduler_type "cosine" \
     --deepspeed $DEEPSPEED_CONFIG \
     --run_name $RUN_NAME \
-    --max_source_length 3072 \
+    --max_source_length 2048 \
     --max_target_length 1024 \
     --overwrite_output_dir \
     --overwrite_cache \
     --logging_strategy "steps" \
     --logging_steps 10 \
     --evaluation_strategy "steps" \
-    --eval_steps 200 \
+    --eval_steps 250 \
     --save_strategy "steps" \
-    --save_steps 200 \
+    --save_steps 250 \
     --seed 1234
