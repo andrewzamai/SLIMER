70741e4ccc791c4a72f6f7a9c0417e0eee04d6e4
Andrew <andrewzamai@hotmail.com>
2024-10-25 11:34:21 +0200

    to mask or not to mask

--- src/SFT_finetuning/evaluating/evaluate_SLIMER_PARALLEL.py
+++ src/SFT_finetuning/evaluating/evaluate_SLIMER_PARALLEL.py
@@ -129,7 +129,7 @@ def load_or_build_dataset_SLIMER_format(datasets_cluster_name, subdataset_name, 
             exclude_misc=True,
             mask_labels=False,
             max_tagNames_per_prompt=-1,
-            input_chunking_window=900,
+            input_chunking_window=300,
             chunking_overlap=15,
         )
         return SLIMER_PARALLEL_dataseDict['test']
@@ -149,8 +149,8 @@ if __name__ == '__main__':
 
     to_eval_on = [
         # converting from uniNER eval datasets using function inside data_handler_pileNER
-        {'datasets_cluster_name': 'crossNER', 'data_handler': data_handler_pileNER, 'subdataset_names': ['ai', 'literature', 'music', 'politics', 'science']},
-        {'datasets_cluster_name': 'MIT', 'data_handler': data_handler_pileNER, 'subdataset_names': ['movie', 'restaurant']},
+        #{'datasets_cluster_name': 'crossNER', 'data_handler': data_handler_pileNER, 'subdataset_names': ['ai', 'literature', 'music', 'politics', 'science']},
+        #{'datasets_cluster_name': 'MIT', 'data_handler': data_handler_pileNER, 'subdataset_names': ['movie', 'restaurant']},
         {'datasets_cluster_name': 'BUSTER', 'data_handler': data_handler_BUSTER, 'subdataset_names': ['BUSTER']},
     ]
 
